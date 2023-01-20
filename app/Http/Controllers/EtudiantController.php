a07f336cc9968900ce818707f0ddfee24b04b292
Maminjatovo <97668363+Maminjatovo@users.noreply.github.com>
2023-01-20 18:32:21 +0300

    modification final

---
+++ app/Http/Controllers/EtudiantController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Etudiant;
+use Illuminate\Http\Request;
+
+class EtudiantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Etudiant::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'prenom' => 'required',
+            'adresse' => 'required',
+            'telephone' => 'required',
+            'sexe' => 'required',
+        ]);
+        $etudiant = Etudiant::create($request->all());
+        return response()->json($etudiant, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $etudiant = Etudiant::findOrFail($id);
+        return response()->json($etudiant);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $etudiant = Etudiant::findOrFail($id);
+        $etudiant->update($request->all());
+        return response()->json($etudiant);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $etudiant = Etudiant::findOrFail($id);
+        $etudiant->delete();
+        return response()->json(null, 204);
+    }
+}
--- app/Models/Etudiant.php
+++ app/Models/Etudiant.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Etudiant extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'sexe',
+        'adresse',
+        'telephone'
+    ];
 }
